458617a2296d6a91f940dc38211d2e29314c579c
Zholud <zholud.zher@mail.ru>
2025-09-11 00:10:08 +0300

    printField feature

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#define FIELD_SIDE 3
 
-int mait() {
+char field[FIELD_SIDE][FIELD_SIDE] = {
+	{' ', ' ', ' '},
+	{' ', ' ', ' '},
+	{' ', ' ', ' '}
+};
 
+void printField() {
+	for (int row = 0; row < FIELD_SIDE; row++) {
+		for (int colum = 0; colum < FIELD_SIDE; colum++) {
+			printf("%c", field[row][colum]);
+			if (colum < 2) { //ToDo: Remove this condition
+				printf("|");
+			}
+		}
+		if (row < 2) {
+			printf("\n-----\n");
+		}
+	}
+}
+
+int main() {
+	printField();
 }
